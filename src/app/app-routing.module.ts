5e9c5b03da3fe9ac2db6aba221a60b82e05f297a
MikeM <Mike.Markus@Ortec-Finance.com>
2021-05-03 16:12:10 +0200

    add a route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AwesomeComponent } from './awesome/awesome.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: '',
+  component: AwesomeComponent
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
